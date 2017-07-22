987082104f9f588805a93aa148e4173a3afb3351
Kelsey Hayes <kelseyhayes@Kelseys-MacBook-Pro.local>
2017-07-22 14:12:34 -0400

    first edits of javascript file

---
+++ assets/javascript/giphyapi.js
@@ -0,0 +1,14 @@
+/*Pseudocode
+-need an array of string of topics
+*/
+var tvShows = ["Game of Thrones", "Outlander",];
+
+/*for (i = 0; i < cars.length; i++) { 
+    text += cars[i] + "<br>";
+}*/
+
+var btn = document.createElement("BUTTON");        // Create a <button> element
+var t = document.createTextNode("CLICK ME");       // Create a text node
+btn.appendChild(t);                                // Append the text to <button>
+document.body.appendChild(btn);                    // Append <button> to <body>
+
